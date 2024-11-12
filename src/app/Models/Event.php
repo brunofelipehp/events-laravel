a8bf367ac834a34061f0c77a953b289f69f7d8b7
Bruno Felipe <brunofelipehp8@gmail.com>
2024-11-12 11:50:33 -0300

    creating relationships between events and users

--- src/app/Models/Event.php
+++ src/app/Models/Event.php
@@ -21,4 +21,9 @@ class Event extends Model
     {
         return $this->belongsTo('App\Model\User');
     }
+
+    public function users()
+    {
+        return $this->belongsToMany('App\Models\User');
+    }
 }
